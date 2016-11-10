04e9762664f170091fd827d39df6ebed10e96e13
Evywell <leduc.axel.pro@gmail.com>
2016-11-10 11:40:26 +0100

    ajout du suivi de paiement

--- controleurs/c_suiviPaiement.php
+++ controleurs/c_suiviPaiement.php
@@ -7,6 +7,22 @@ switch($action){
     case 'demandeSuiviPaiement':
         $fiches = $pdo->getFichesValides();
 
+        // Une demande de suivi de fiche a été saisie
+        if(isset($_GET['fiche'])){
+            $infos = explode('-', $_GET['fiche']);
+            if(isset($infos[0]) && isset($infos[1])){
+                $laFiche['forfait'] = $pdo->getLesFraisForfait($infos[1], $infos[0]);
+                $laFiche['hors_forfait'] = $pdo->getLesFraisHorsForfait($infos[1], $infos[0]);
+            }else {
+                $laFiche['forfait'] = $laFiche['hors_forfait'] = [];
+            }
+
+            if(empty($laFiche['forfait']) && empty($laFiche['hors_forfait'])){
+                setFlash("La fiche demandée n'existe pas");
+                header('location:index.php?uc=suiviPaiement&action=demandeSuiviPaiement');
+                die();
+            }
+        }
 
         require "vues/v_demandeSuiviPaiement.php";
         break;
--- vues/v_demandeSuiviPaiement.php
+++ vues/v_demandeSuiviPaiement.php
@@ -1,6 +1,53 @@
-<h2>Liste des fiches de frais</h2>
-<select name="" id="">
-    <?php foreach($fiches as $fiche): ?>
-        <option value="<?= $fiche['mois'] . '-' . $fiche['idvisiteur'] ?>"><?=  substr($fiche['mois'], 4, 2) . ' - ' . $fiche['nom'] . ' ' . $fiche['prenom']; ?></option>
-    <?php endforeach; ?>
-</select>
+<div id="contenu">
+    <h2>Liste des fiches de frais</h2>
+    <form method="GET" action="index.php">
+        <input type="hidden" name="uc" value="suiviPaiement">
+        <input type="hidden" name="action" value="demandeSuiviPaiement">
+        <select name="fiche">
+            <?php foreach($fiches as $fiche): ?>
+                <option value="<?= $fiche['mois'] . '-' . $fiche['idvisiteur'] ?>"><?=  substr($fiche['mois'], 4, 2) . ' - ' . $fiche['nom'] . ' ' . $fiche['prenom']; ?></option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
+    <?php if(isset($laFiche)): ?>
+        <h3>Les frais hors forfait</h3>
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <th>Libellé</th>
+                    <th>Montant</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($laFiche['hors_forfait'] as $value): ?>
+                <tr>
+                    <td><?= $value['libelle'] ?></td>
+                    <td><?= $value['montant'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <h3>Les frais forfaitaires</h3>
+        <table style="width:100%">
+            <thead>
+            <tr>
+                <th>Libellé</th>
+                <th>Quantité</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($laFiche['forfait'] as $value): ?>
+                <tr>
+                    <td><?= $value['libelle'] ?></td>
+                    <td><?= $value['quantite'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+        <p>
+            <h3><a href="">Télécharger au format PDF</a></h3>
+        </p>
+    <?php endif; ?>
+</div>
